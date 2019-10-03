3b3000e55cb99a3664c3958db0a2eec86b288fe8
miroslawmajka <go2mirek@gmail.com>
2019-10-03 21:42:58 +0100

    Adde additional samples page and published

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const TITLE = { title: 'Sample Express Web Application' };
+
 /* GET home page */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Sample Express Web Application' });
+    res.render('index', TITLE);
+});
+
+router.get('/additional-samples', (req, res, next) => {
+    res.render('additional-samples', TITLE);
 });
 
 module.exports = router;
